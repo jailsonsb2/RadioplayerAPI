8cd3f23670ae3414d8e879410d4e812bead29f07
Jailson Bezerra <jailson.sb2@gmail.com>
2024-07-01 23:10:51 -0300

    fix

--- js/main.js
+++ js/main.js
@@ -136,7 +136,7 @@
           }
           const API_URL = `https://api.streamafrica.net/new.search.php?query=${encodeURIComponent(text)}&service=spotify`;
           const response = await fetch(API_URL);
-          if (title === "LA JEFA GREENVILLE" || response.status === 403) {
+          if (title === "Radioplayer Demo" || response.status === 403) {
                   const results = {
                           title,
                           artist,
